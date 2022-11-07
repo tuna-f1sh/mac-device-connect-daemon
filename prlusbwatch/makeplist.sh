44ae076b2bdbd703131e89a47b833e542dd4ac3b
John Whittington <git@jbrengineering.co.uk>
2022-11-07 19:38:10 +0100

    fix makeplsit

--- prlusbwatch/makeplist.sh
+++ prlusbwatch/makeplist.sh
@@ -21,18 +21,18 @@ pid=$4
 
 if [[ "$vid" =~ ^0x[0-9a-zA-Z]{4}$ ]]; then
   vid_hex=$vid
-  vid_dec=$((16#$vid))
+  vid_dec=$((16#${vid//0x/}))
 else
   vid_dec=$vid
-  vid_hex=$(printf '%04x' "$vid")
+  vid_hex=$(printf '0x%04x' "$vid")
 fi
 
 if [[ "$pid" =~ ^0x[0-9a-zA-Z]{4}$ ]]; then
   pid_hex=$pid
-  pid_dec=$((16#$pid))
+  pid_dec=$((16#${pid//0x/}))
 else
   pid_dec=$pid
-  pid_hex=$(printf '%04x' "$pid")
+  pid_hex=$(printf '0x%04x' "$pid")
 fi
 
 # get UUID of guest if not a UUID now rather than every call to script
@@ -43,7 +43,7 @@ if ! [[ "$guest" =~ ^[0-9a-zA-Z]{8}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{4}
     echo "Failed to get guest UUID"
     exit 1
   else
-    echo "guest UUID $guest"
+    echo "guest UUID $uuid"
   fi
 else
   uuid=$guest
@@ -52,8 +52,8 @@ fi
 cp prlusbwatch/com.prlusbwatch.template prlusbwatch/com.prlusbwatch."$device".plist
 sed -i '' "s/\$VID_DEC/$vid_dec/g" prlusbwatch/com.prlusbwatch."$device".plist
 sed -i '' "s/\$PID_DEC/$pid_dec/g" prlusbwatch/com.prlusbwatch."$device".plist
-sed -i '' "s/\$VID_HEX/0x$vid_hex/g" prlusbwatch/com.prlusbwatch."$device".plist
-sed -i '' "s/\$PID_HEX/0x$pid_hex/g" prlusbwatch/com.prlusbwatch."$device".plist
+sed -i '' "s/\$VID_HEX/$vid_hex/g" prlusbwatch/com.prlusbwatch."$device".plist
+sed -i '' "s/\$PID_HEX/$pid_hex/g" prlusbwatch/com.prlusbwatch."$device".plist
 sed -i '' "s/\$GUEST/$uuid/g" prlusbwatch/com.prlusbwatch."$device".plist
 sed -i '' "s/\$DEV/$device/g" prlusbwatch/com.prlusbwatch."$device".plist
 echo "Made prlusbwatch/com.prlusbwatch.$device.plist to auto-connect VID/PID 0x$vid_hex/0x$pid_hex ($device) to $guest"
